670fbbace192d6912a5c55cab8353196b9c10b94
kendimuthomi <kendijackline42@gmail.com>
2023-03-29 11:18:47 +0300

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+def canUnlockAll(boxes):
+    """Method that determines if all boxes can be unlocked"""
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key >= 0 and key < n and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
+
+    return all(unlocked)
